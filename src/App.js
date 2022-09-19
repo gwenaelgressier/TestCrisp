8f7d25b24f296e157d7a2de08eb7969e301b62c8
gwenaelgressier <97903290+gwenaelgressier@users.noreply.github.com>
2022-09-19 22:38:53 +0200

    v1

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect } from "react";
 
 function App() {
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.src = "https://client.crisp.chat/l.js";
+    script.async = true;
+    window.$crisp = [];
+    window.CRISP_WEBSITE_ID = "981398da-8b7c-4267-956f-787c555d1431";
+    document.getElementsByTagName("head")[0].appendChild(script);
+    window.$crisp.push(["do", "chat:hide"]);
+  }, []);
+
+  useEffect(() => {
+    window.$crisp?.push(["set", "user:nickname", ["malienda"]]);
+    window.$crisp?.push(["set", "user:email", ["gressier.gwenael@gmail.com"]]);
+    window.$crisp?.push(["do", "chat:show"]);
+    return () => {
+      window.$crisp?.push(["do", "chat:hide"]);
+    };
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
